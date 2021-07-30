74e9cd35ce026b8a2bae0359915f3a01d2c880f2
nosokina <nosokina@zuzex.com>
2021-07-30 12:27:36 +0300

    broken test adding

--- test_allureex.py
+++ test_allureex.py
@@ -2,6 +2,7 @@ from allure_commons.types import AttachmentType
 from selenium import webdriver
 import allure
 import pytest
+import time
 
 
 @allure.severity(allure.severity_level.NORMAL)
@@ -37,5 +38,24 @@ class TestHRM:
                 assert True
             else:
                 allure.attach(self.driver.get_screenshot_as_png(), name="LoginScreen-30-07", attachment_type=(AttachmentType.PNG))
+                self.driver.close()
+                assert False
+
+        @allure.severity(allure.severity_level.NORMAL)
+        def test_info(self):
+            self.driver = webdriver.Chrome()
+            self.driver.get("https://opensource-demo.orangehrmlive.com/")
+            self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+            self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+            self.driver.find_element_by_id("btnLogin").click()
+            time.sleep(5)
+            self.driver.find_element_by_link_text("My Info")
+            act_url=self.driver.getCurrentUrl() # тест помечен в отчёте аллюра как Broken - AttributeError: 'WebDriver' object has no attribute 'getCurrentUrl'
+
+            if act_url == "https://opensource-demo.orangehrmlive.com/index.php/dashboard":
+                allure.attach(self.driver.get_screenshot_as_png(), name="MyInfo-30-07", attachment_type=(AttachmentType.PNG))
+                self.driver.close()
+                assert True
+            else:
                 self.driver.close()
                 assert False
